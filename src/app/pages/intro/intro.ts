156452377e90be3243a9639b97ad225aaadb8770
Derrick Similien <dsimili1@my.westga.edu>
2026-02-26 19:39:58 -0500

    Auto-navigate from Intro to Landing

--- src/app/pages/intro/intro.ts
+++ src/app/pages/intro/intro.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-intro',
-  imports: [],
+  standalone: true,
   templateUrl: './intro.html',
   styleUrl: './intro.scss',
 })
 export class Intro {
-
+  constructor(private router: Router) {
+    setTimeout(() => {
+      this.router.navigateByUrl('/home'); // or '/' depending on what you chose above
+    }, 2500);
+  }
 }
